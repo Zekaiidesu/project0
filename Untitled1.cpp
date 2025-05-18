f02aa7dd134eec4f1aa5aa4c4f73f157bd7fec5a
Zekaiidesu <mzakyilham10@gmail.com>
2025-05-18 10:14:44 +0700

    ok

---
+++ Untitled1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int a,b;
+    cout << "nilai pertama : ";
+    cin >> a;
+    cout << "nilai kedua : ";
+    cin >> b;
+    }
---
+++ oi.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main () {
+	int i,j;
+	for (i=1; i<=3; i++){
+	for (j=1; j<=i; j++){
+		cout << char (64+j);
+}
+cout <<endl;
+
+}
+	for (i=2; i>=1; i--){
+	for (j=1; j<=i; j++){
+		cout << char (64+j);
+}
+}
+}
